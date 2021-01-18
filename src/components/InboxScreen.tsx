4b8403b6f71cc2f41f906be430c612f1113ada81
Marsen <thisismysoul@gmail.com>
2021-01-18 15:46:40 +0800

    add page InboxScreen

---
+++ src/components/InboxScreen.tsx
@@ -0,0 +1,36 @@
+//src/components/InboxScreen.js
+
+import React from 'react';
+import { connect } from 'react-redux';
+import TaskList from './TaskList';
+
+export class InboxScreenArgs {
+  error:string | undefined 
+}
+
+export function PureInboxScreen(args:InboxScreenArgs) {
+  if (args.error) {
+    return (
+      <div className="page lists-show">
+        <div className="wrapper-message">
+          <span className="icon-face-sad" />
+          <div className="title-message">Oh no!</div>
+          <div className="subtitle-message">Something went wrong</div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="page lists-show">
+      <nav>
+        <h1 className="title-page">
+          <span className="title-wrapper">TaskBox</span>
+        </h1>
+      </nav>
+      <TaskList />
+    </div>
+  );
+}
+
+export default connect((props:InboxScreenArgs) => (props))(PureInboxScreen);
